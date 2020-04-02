ad298b0e0f7190086a9e4145cee872b77e1c45f4
Aruksh Patel <arukshpatel98@gmail.com>
2020-04-02 16:46:31 -0500

    updates

--- script/script.js
+++ script/script.js
@@ -3,33 +3,11 @@ function getCSV() {
         var responseIn = this.responseText;
 
         console.log(responseIn);
+        
+        alert(responseIn);
     }
     var request = new XMLHttpRequest();
     request.onload = printSys;
     request.open('get', 'https://arukshpatel.com/Lineage_Tree/res/david_le.csv', true);
-    request.send();
-}
-
-function getDavidLe() {
-    function printSys() {
-        var responseIn = this.responseText;
-
-        console.log(responseIn);
-    }
-    var request = new XMLHttpRequest();
-    request.onload = printSys;
-    request.open('get', 'https://arukshpatel.com/Lineage_Tree/res/david_le.csv', true);
-    request.send();
-}
-
-function getMinhLe() {
-    function printSys() {
-        var responseIn = this.responseText;
-
-        console.log(responseIn);
-    }
-    var request = new XMLHttpRequest();
-    request.onload = printSys;
-    request.open('get', 'https://arukshpatel.com/Lineage_Tree/res/minh_le.csv', true);
     request.send();
 }
