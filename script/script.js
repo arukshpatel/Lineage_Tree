1adb9e3668db69a6dc3486bafba9ff70bbb01268
Aruksh Patel <arukshpatel98@gmail.com>
2020-04-02 17:45:44 -0500

    csv to json function added

--- script/script.js
+++ script/script.js
@@ -2,12 +2,31 @@ function getCSV() {
     function printSys() {
         var responseIn = this.responseText;
 
-        console.log(responseIn);
-        
-        alert(responseIn);
+        // console.log(responseIn);
+
+        var outJSON = csvJSON(responseIn);
+
+        console.log(outJSON);
+        // alert(responseIn);
     }
     var request = new XMLHttpRequest();
     request.onload = printSys;
     request.open('get', 'https://arukshpatel.com/Lineage_Tree/res/david_le.csv', true);
     request.send();
+}
+
+function csvJSON(csv) {
+    var lines = csv.split("\n");
+    var result = [];
+    var headers = lines[0].split(",");
+    for (var i = 1; i < lines.length; i++) {
+        var obj = {};
+        var currentline = lines[i].split(",");
+        for (var j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
 }
