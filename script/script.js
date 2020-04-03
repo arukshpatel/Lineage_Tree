c830d4eadd987c76932ea85d5916b072ddbc552f
Aruksh Patel <arukshpatel98@gmail.com>
2020-04-02 22:21:16 -0500

    updates

--- script/script.js
+++ script/script.js
@@ -1,35 +1,48 @@
 var json;
 
+var member = {
+    name,
+    lineNum,
+    bigLineNum,
+    children : [],
+    addChildren : function(littleLineNum) {
+        this.children.push(littleLineNum);
+    }
+
+};
 
 function getCSV(lineage) {
     var request = new XMLHttpRequest();
     request.onload = printSys;
-    request.open('get', 'https://arukshpatel.com/Lineage_Tree/res/'+ lineage + '.csv', true);
+    request.open('get', 'https://arukshpatel.com/Lineage_Tree/res/' + lineage + '.csv', true);
     request.send();
+    createArray();
 
     function printSys() {
         var responseIn = this.responseText;
-
         json = csvJSON(responseIn);
-
         document.getElementById('tree').innerHTML = json[0].id;
+    }
+
+    function csvJSON(csv) {
+        var lines = csv.split("\n");
+        var result = [];
+        var headers = lines[0].split(",");
+        for (var i = 1; i < lines.length; i++) {
+            var obj = {};
+            var currentline = lines[i].split(",");
+            for (var j = 0; j < headers.length; j++) {
+                obj[headers[j]] = currentline[j];
+            }
+            result.push(obj);
+        }
+
+        return JSON.parse(JSON.stringify(result));
     }
 }
 
-function csvJSON(csv) {
-    var lines = csv.split("\n");
-    var result = [];
-    var headers = lines[0].split(",");
-    for (var i = 1; i < lines.length; i++) {
-        var obj = {};
-        var currentline = lines[i].split(",");
-        for (var j = 0; j < headers.length; j++) {
-            obj[headers[j]] = currentline[j];
-        }
-        result.push(obj);
-    }
+function createArray() {
 
-    return JSON.parse(JSON.stringify(result));
 }
 
 function print() {
